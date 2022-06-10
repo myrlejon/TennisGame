85d39d79c61edd8d44ddb26dd51146a38286bb03
myrlejon <60348075+myrlejon@users.noreply.github.com>
2022-06-10 08:30:48 +0200

    Refactored method and refined window.

--- TennisGame/Form1.Designer.cs
+++ TennisGame/Form1.Designer.cs
@@ -35,11 +35,15 @@ namespace TennisGame
             this.player2ScoreLabel = new System.Windows.Forms.Label();
             this.player1Score = new System.Windows.Forms.Label();
             this.player2Score = new System.Windows.Forms.Label();
+            this.player1ScoreString = new System.Windows.Forms.Label();
+            this.player2ScoreString = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // player1
             // 
-            this.player1.Location = new System.Drawing.Point(98, 320);
+            this.player1.Location = new System.Drawing.Point(126, 320);
             this.player1.Name = "player1";
             this.player1.Size = new System.Drawing.Size(129, 68);
             this.player1.TabIndex = 0;
@@ -49,7 +53,7 @@ namespace TennisGame
             // 
             // player2
             // 
-            this.player2.Location = new System.Drawing.Point(667, 320);
+            this.player2.Location = new System.Drawing.Point(486, 320);
             this.player2.Name = "player2";
             this.player2.Size = new System.Drawing.Size(129, 68);
             this.player2.TabIndex = 1;
@@ -60,7 +64,7 @@ namespace TennisGame
             // player1ScoreLabel
             // 
             this.player1ScoreLabel.AutoSize = true;
-            this.player1ScoreLabel.Location = new System.Drawing.Point(142, 391);
+            this.player1ScoreLabel.Location = new System.Drawing.Point(165, 390);
             this.player1ScoreLabel.Name = "player1ScoreLabel";
             this.player1ScoreLabel.Size = new System.Drawing.Size(41, 15);
             this.player1ScoreLabel.TabIndex = 2;
@@ -69,7 +73,7 @@ namespace TennisGame
             // player2ScoreLabel
             // 
             this.player2ScoreLabel.AutoSize = true;
-            this.player2ScoreLabel.Location = new System.Drawing.Point(712, 391);
+            this.player2ScoreLabel.Location = new System.Drawing.Point(529, 391);
             this.player2ScoreLabel.Name = "player2ScoreLabel";
             this.player2ScoreLabel.Size = new System.Drawing.Size(41, 15);
             this.player2ScoreLabel.TabIndex = 3;
@@ -78,7 +82,7 @@ namespace TennisGame
             // player1Score
             // 
             this.player1Score.AutoSize = true;
-            this.player1Score.Location = new System.Drawing.Point(179, 391);
+            this.player1Score.Location = new System.Drawing.Point(203, 391);
             this.player1Score.Name = "player1Score";
             this.player1Score.Size = new System.Drawing.Size(13, 15);
             this.player1Score.TabIndex = 4;
@@ -87,18 +91,49 @@ namespace TennisGame
             // player2Score
             // 
             this.player2Score.AutoSize = true;
-            this.player2Score.Location = new System.Drawing.Point(749, 390);
+            this.player2Score.Location = new System.Drawing.Point(566, 391);
             this.player2Score.Name = "player2Score";
             this.player2Score.Size = new System.Drawing.Size(13, 15);
             this.player2Score.TabIndex = 5;
             this.player2Score.Text = "0";
+            // 
+            // player1ScoreString
+            // 
+            this.player1ScoreString.AutoSize = true;
+            this.player1ScoreString.Location = new System.Drawing.Point(174, 302);
+            this.player1ScoreString.Name = "player1ScoreString";
+            this.player1ScoreString.Size = new System.Drawing.Size(32, 15);
+            this.player1ScoreString.TabIndex = 6;
+            this.player1ScoreString.Text = "Love";
+            // 
+            // player2ScoreString
+            // 
+            this.player2ScoreString.AutoSize = true;
+            this.player2ScoreString.Location = new System.Drawing.Point(538, 302);
+            this.player2ScoreString.Name = "player2ScoreString";
+            this.player2ScoreString.Size = new System.Drawing.Size(32, 15);
+            this.player2ScoreString.TabIndex = 7;
+            this.player2ScoreString.Text = "Love";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::TennisGame.Properties.Resources.tennis_icon_6;
+            this.pictureBox1.Location = new System.Drawing.Point(268, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(171, 159);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.ForestGreen;
-            this.ClientSize = new System.Drawing.Size(912, 450);
+            this.ClientSize = new System.Drawing.Size(708, 471);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.player2ScoreString);
+            this.Controls.Add(this.player1ScoreString);
             this.Controls.Add(this.player2Score);
             this.Controls.Add(this.player1Score);
             this.Controls.Add(this.player2ScoreLabel);
@@ -107,6 +142,7 @@ namespace TennisGame
             this.Controls.Add(this.player1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -114,12 +150,16 @@ namespace TennisGame
 
         #endregion
 
+
         private System.Windows.Forms.Button player1;
         private System.Windows.Forms.Button player2;
         private System.Windows.Forms.Label player1ScoreLabel;
         private System.Windows.Forms.Label player2ScoreLabel;
         private System.Windows.Forms.Label player1Score;
         private System.Windows.Forms.Label player2Score;
+        private System.Windows.Forms.Label player1ScoreString;
+        private System.Windows.Forms.Label player2ScoreString;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- TennisGame/Form1.cs
+++ TennisGame/Form1.cs
@@ -14,22 +14,43 @@ namespace TennisGame
     {
         private int PlayerOneScoreCounter { get; set; }
         private int PlayerTwoScoreCounter { get; set; }
+        private Dictionary<string, int> Scores { get; set; }
 
         public Form1()
         {
+            Scores = new Dictionary<string, int>()
+            {
+                {"Love", 0},
+                {"Fifteen", 1},
+                {"Thirty", 2},
+                {"Forty", 3}
+            };
+
             InitializeComponent();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            PlayerOneScoreCounter++;
-            player1Score.Text = PlayerOneScoreCounter.ToString();
+            PlayerOneScoreCounter = ButtonHandler(PlayerOneScoreCounter, player1Score, player1ScoreString);
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            PlayerTwoScoreCounter++;
-            player2Score.Text = PlayerTwoScoreCounter.ToString();
+            PlayerTwoScoreCounter = ButtonHandler(PlayerTwoScoreCounter, player2Score, player2ScoreString);
         }
+
+        private int ButtonHandler(int counter, Label playerScore, Label playerScoreString)
+        {
+            counter++;
+            playerScore.Text = counter.ToString();
+            foreach (var key in Scores)
+            {
+                if (key.Value == counter)
+                {
+                    playerScoreString.Text = key.Key;
+                }
+            }
+            return counter;
+        } 
     }
 }
