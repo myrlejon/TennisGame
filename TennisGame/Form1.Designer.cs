17350733703e429ad3f276eb2becbf4ac0fcbc14
myrlejon <60348075+myrlejon@users.noreply.github.com>
2022-06-10 07:30:14 +0200

    Added basic design to forms window.

--- TennisGame/Form1.Designer.cs
+++ TennisGame/Form1.Designer.cs
@@ -29,13 +29,97 @@ namespace TennisGame
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.player1 = new System.Windows.Forms.Button();
+            this.player2 = new System.Windows.Forms.Button();
+            this.player1ScoreLabel = new System.Windows.Forms.Label();
+            this.player2ScoreLabel = new System.Windows.Forms.Label();
+            this.player1Score = new System.Windows.Forms.Label();
+            this.player2Score = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // player1
+            // 
+            this.player1.Location = new System.Drawing.Point(98, 320);
+            this.player1.Name = "player1";
+            this.player1.Size = new System.Drawing.Size(129, 68);
+            this.player1.TabIndex = 0;
+            this.player1.Text = "Player 1";
+            this.player1.UseVisualStyleBackColor = true;
+            this.player1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // player2
+            // 
+            this.player2.Location = new System.Drawing.Point(667, 320);
+            this.player2.Name = "player2";
+            this.player2.Size = new System.Drawing.Size(129, 68);
+            this.player2.TabIndex = 1;
+            this.player2.Text = "Player 2";
+            this.player2.UseVisualStyleBackColor = true;
+            this.player2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // player1ScoreLabel
+            // 
+            this.player1ScoreLabel.AutoSize = true;
+            this.player1ScoreLabel.Location = new System.Drawing.Point(142, 391);
+            this.player1ScoreLabel.Name = "player1ScoreLabel";
+            this.player1ScoreLabel.Size = new System.Drawing.Size(41, 15);
+            this.player1ScoreLabel.TabIndex = 2;
+            this.player1ScoreLabel.Text = "score: ";
+            // 
+            // player2ScoreLabel
+            // 
+            this.player2ScoreLabel.AutoSize = true;
+            this.player2ScoreLabel.Location = new System.Drawing.Point(712, 391);
+            this.player2ScoreLabel.Name = "player2ScoreLabel";
+            this.player2ScoreLabel.Size = new System.Drawing.Size(41, 15);
+            this.player2ScoreLabel.TabIndex = 3;
+            this.player2ScoreLabel.Text = "score: ";
+            // 
+            // player1Score
+            // 
+            this.player1Score.AutoSize = true;
+            this.player1Score.Location = new System.Drawing.Point(179, 391);
+            this.player1Score.Name = "player1Score";
+            this.player1Score.Size = new System.Drawing.Size(13, 15);
+            this.player1Score.TabIndex = 4;
+            this.player1Score.Text = "0";
+            // 
+            // player2Score
+            // 
+            this.player2Score.AutoSize = true;
+            this.player2Score.Location = new System.Drawing.Point(749, 390);
+            this.player2Score.Name = "player2Score";
+            this.player2Score.Size = new System.Drawing.Size(13, 15);
+            this.player2Score.TabIndex = 5;
+            this.player2Score.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.ForestGreen;
+            this.ClientSize = new System.Drawing.Size(912, 450);
+            this.Controls.Add(this.player2Score);
+            this.Controls.Add(this.player1Score);
+            this.Controls.Add(this.player2ScoreLabel);
+            this.Controls.Add(this.player1ScoreLabel);
+            this.Controls.Add(this.player2);
+            this.Controls.Add(this.player1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button player1;
+        private System.Windows.Forms.Button player2;
+        private System.Windows.Forms.Label player1ScoreLabel;
+        private System.Windows.Forms.Label player2ScoreLabel;
+        private System.Windows.Forms.Label player1Score;
+        private System.Windows.Forms.Label player2Score;
     }
 }
 
--- TennisGame/Form1.cs
+++ TennisGame/Form1.cs
@@ -12,10 +12,24 @@ namespace TennisGame
 {
     public partial class Form1 : Form
     {
+        private int PlayerOneScoreCounter { get; set; }
+        private int PlayerTwoScoreCounter { get; set; }
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PlayerOneScoreCounter++;
+            player1Score.Text = PlayerOneScoreCounter.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            PlayerTwoScoreCounter++;
+            player2Score.Text = PlayerTwoScoreCounter.ToString();
+        }
     }
 }
